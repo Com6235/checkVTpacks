ec4d4bce33152033e97d02810fd22b814df08ba3
Com6235 <ru.nms199@gmail.com>
2024-08-28 23:05:51 +0300

    Actually fix the code (almost)

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,11 +1,11 @@
 package io.github.com6235
 
 import com.charleskorn.kaml.Yaml
+import com.charleskorn.kaml.YamlConfiguration
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.json.Json
 import java.io.File
 import java.io.FileOutputStream
-import java.io.IOException
 import java.net.URI
 import java.net.http.HttpClient
 import java.net.http.HttpRequest
@@ -19,23 +19,30 @@ import kotlin.io.path.extension
 import kotlin.io.path.readLines
 
 @Serializable
-data class SelectedPacks(val Version: String, val Packs: List<String>, val `Combined packs`: List<String>)
+data class SelectedPacks(
+    val Version: String,
+    val Packs: List<String>? = listOf(),
+    val `Combined packs`: List<String>? = listOf()
+)
+
 @Serializable
 data class BadPacks(val badpacks: List<String>)
 
 val lang = ResourceBundle.getBundle("lang")
+
 val badSelected by lazy {
     Json.decodeFromString<BadPacks>(
         HttpClient.newHttpClient()
             .send(
-                HttpRequest.newBuilder(URI.create("https://raw.githubusercontent.com/Com6235/checkVTpacks/meta/badpacks.json"))
-                    .GET()
-                    .build(),
+                HttpRequest.newBuilder(
+                    URI.create("https://raw.githubusercontent.com/Com6235/checkVTpacks/meta/badpacks.json")
+                ).GET().build(),
                 HttpResponse.BodyHandlers.ofString()
             ).body()
     )
 
 }
+
 fun translated(key: String, vararg args: Any): String = lang.getString(key).format(*args)
 
 fun main(args: Array<String>) {
@@ -44,9 +51,10 @@ fun main(args: Array<String>) {
 
     val out = when (path.extension.lowercase()) {
         "txt" -> parseTxt(path)
+        // TODO fix zip archives
         "zip" -> {
-            val pwd = Path(System.getenv("user.dir"))
-            unzip(pwd.toFile(), Path(System.getenv("user.dir")))
+            val pwd = Path(System.getProperty("user.dir"))
+            unzip(path.toFile(), pwd)
 
             val file = Path(pwd.absolutePathString(), "Selected Packs.txt")
             parseTxt(file)
@@ -65,33 +73,27 @@ fun main(args: Array<String>) {
     }
 }
 
-fun parseTxt(path: Path): List<String> {
+fun parseTxt(path: Path): List<String>? {
     val lines = path.readLines()
     if (lines[0] != "Vanilla Tweaks Resource Pack") {
         println(translated("bad-txt", path.absolutePathString()))
         return listOf()
     }
 
-    val yaml = Yaml.default.decodeFromString(SelectedPacks.serializer(), lines.subList(1, lines.size).joinToString("\n").replace("\t", "  - "))
-    return yaml.Packs.filter { it in badSelected.badpacks }
+    val yaml = Yaml(configuration = YamlConfiguration(strictMode = false)).decodeFromString(SelectedPacks.serializer(), lines.subList(1, lines.size).joinToString("\n").replace("\t", "  - "))
+    return yaml.Packs?.filter { it in badSelected.badpacks }
 }
 
 fun unzip(zipFile: File, output: Path) {
-    val buffer = ByteArray(1024)
+    val buffer = ByteArray(16384)
     val zis = ZipInputStream(zipFile.inputStream())
     var zipEntry = zis.nextEntry
     while (zipEntry != null) {
+        if (zipEntry.name != "Selected Packs.txt") return
         val newFile = File(output.toFile(), zipEntry.name)
         if (zipEntry.isDirectory) {
-            if (!newFile.isDirectory && !newFile.mkdirs()) {
-                throw IOException("Failed to create directory $newFile")
-            }
+            continue
         } else {
-            val parent = newFile.parentFile
-            if (!parent.isDirectory && !parent.mkdirs()) {
-                throw IOException("Failed to create directory $parent")
-            }
-
             val fos = FileOutputStream(newFile)
             var len: Int
             while ((zis.read(buffer).also { len = it }) > 0) {
